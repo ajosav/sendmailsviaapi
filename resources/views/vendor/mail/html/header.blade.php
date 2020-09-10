7427298dffd21a725d3b3073d37254fce395c650
ajosav <ajosavboy@gmail.com>
2020-09-10 18:20:42 +0100

    Removed html layouts so it can house any html

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,1 @@
-<tr>
-<td class="header">
-{{-- <a href="{{ $url }}" style="display: inline-block;"> --}}
-@if (trim($slot) === '')
-<img src="{{URL::asset('renmoney_logo.svg')}}" class="logo" alt="Renmoney" data-auto-embed="attachment">
-@else
-{{ $slot }}
-@endif
-{{-- </a> --}}
-</td>
-</tr>
+
